54904267febeaf4e91df22b8bc882dc135bda2a5
ebrima <ebrima.jobe92@gmail.com>
2021-08-01 11:02:22 -0700

    scan tix pt.1

---
+++ pages/api/scanTix.ts
@@ -0,0 +1,23 @@
+import { sendEmail, wrapAsync } from './helpers';
+import { OrderProps, EventProps } from '../../src/@types/types';
+import { NextApiRequest } from 'next';
+
+const updateOrder = async (
+  order: OrderProps,
+  event: EventProps,
+  db: any
+) => {
+  await db
+    .collection('event')
+    .updateOne({ _id: event._id }, { $set: event });
+  return await db
+    .collection('order')
+    .updateOne({ _id: order._id }, { $set: order });  
+};
+
+export default wrapAsync(async (req: NextApiRequest, db: any) => {
+  const { event, order }: { event: EventProps; order: OrderProps } = req.body;
+  await updateOrder(order, event, db);
+  await sendEmail([order.emailAddress], event, order);
+  return true;
+});
--- src/components/Dashboard/ManageEvent/ScanTicket.tsx
+++ src/components/Dashboard/ManageEvent/ScanTicket.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
-import { EventProps, OrderProps } from '../../../@types/types';
+import { EventProps, OrderProps, UserTicketProps } from '../../../@types/types';
 import moment from 'moment';
 import classnames from 'classnames';
-import { formatEventDateTime } from '../../../lib';
+import { formatDate, formatEventDateTime, formatEventTime, getOrderTicketCount } from '../../../lib';
+import QRCode from 'qrcode.react';
+
 const importQR = () => import('react-qr-reader');
 
 const QrReader = dynamic(importQR, {
@@ -18,22 +20,33 @@ export const ScanTicket: React.FunctionComponent<Props> = ({
   event,
   orders,
 }) => {
-  const [result, setResult] = useState<string>('');
-  const [tickets] = useState<any>(
+  const [tickets] = useState<UserTicketProps[]>(
     orders.reduce((acc, curr) => {
       return acc.concat(curr.tickets);
     }, [])
   );
   const [live] = useState<boolean>(new Date(event.startDate) > new Date());
   const [facing, setFacing] = useState<boolean>(true);
+  const [tixMode, setTixMode] = useState<boolean>(false);
+  const [order, setOrder] = useState<OrderProps>(null);
+  const [tix, setTix] = useState<UserTicketProps>(null);
+
   const [inProgress] = useState<boolean>(
     moment().isBetween(event.startDate, event.endDate)
   );
-  console.log(tickets);
+  console.log(orders)
   const handleScan = (data) => {
+    console.log('dude',data);
+    
     if (data) {
-      setResult(data);
-      console.log(tickets.filter((curr) => curr.barCode === data));
+      const tix = (tickets.filter((curr) => curr.barCode === data))[0];
+      if (tix){
+        setTix(tix)
+        const order = orders.filter((curr)=> curr._id === tix.orderId)[0]
+        setOrder(order)
+        setTixMode(true)
+        console.log('bro',tix, order);
+      }
     }
   };
   const handleError = (err) => {
@@ -46,7 +59,7 @@ export const ScanTicket: React.FunctionComponent<Props> = ({
         <article className="dt tc tl-ns w-90-l w-100-m  pb2 mv2">
           <div className="dtc-l dtc-m  pt2-m pb2 v-mid  fw7">
             <div className="mb3">
-              <span className="f3-l f4 fw6-l fw4 br-100 b--solid pv2 ph3 mv2">
+              <span className="ba bw2 f3-l f4 fw6-l fw4 br-100 b--solid pv2 ph3 mv2">
                 Scan Tickets
               </span>
             </div>
@@ -84,8 +97,16 @@ export const ScanTicket: React.FunctionComponent<Props> = ({
           <div className="w-auto-m dtc" />
         </article>
       </main>
+      {!tixMode?
       <div className="w-70-ns w-90 center">
-        <div className="mb3"></div>
+        <div className="mb3">
+          <span
+            onClick={() => setFacing(!facing)}
+            className="ba bw2 f3-l f4 fw6-l fw4 br-100 b--solid pv2 ph3 mv2"
+          >
+            Flip Camera
+          </span>
+        </div>
         <QrReader
           delay={300}
           resolution={790}
@@ -94,14 +115,91 @@ export const ScanTicket: React.FunctionComponent<Props> = ({
           onScan={handleScan}
           style={{ width: '100%' }}
         />
-        <span
-          onClick={() => setFacing(!facing)}
-          className="f3-l f4 fw6-l fw4 br-100 b--solid pv2 ph3 mv2"
-        >
-          Flip Camera
-        </span>
-      </div>
-      <p>{result}</p>
+      </div>:
+      <div>
+        <main className="mw9 ml4-ns ph3-l center">
+          <article className="dt tc tl-ns w-90-l w-100-m  pb2 mv2">
+            <div className="dtc-l dtc-m  pt2-m pb2 v-mid  fw7">
+              <div className="mb3 bg-black-80">
+                <span className="f3-l f4 fw6-l fw4 ba bw2  br-100 b--solid pv2 ph3 mv2">
+                  Order Details
+                </span>
+              </div>
+              <div className=" lh-title f3 mb0 mt0-ns underline-hover">
+                {`${order.firstName} ${order.lastName}`}
+              </div>
+              <div className="f4-ns f5 fw6 lh-title mv0 underline-hover">
+                {order.emailAddress}
+              </div>
+              <div>
+                <span className="f4-ns f5 fw6 mv0 gray">
+                  Purchase Date: {`${formatDate(order.orderDate, 'medium')}`}
+                </span>
+              </div>
+            </div>
+            <div className="w-auto-m dtc" />
+          </article>
+        
+          <div className=" mt2 flex flex-wrap">
+            <div
+              className="w-100 center ma2 black bg-light-gray h-25 fl relative pa2 mt1 bt w-80-ns"
+              style={{
+                borderRadius: '8px',
+              }}
+            >
+              <h3
+                className="mv2 pv3"
+                style={{
+                  background:
+                    `linear-gradient(to bottom, #e84c3d 0%, #e84c3d 26%, #ecedef 26%, #ecedef 100%)`,
+                }}
+              >
+                Social <span className="normal">Ticketing</span>
+              </h3>
+              <div className="fl">
+                <div className="ttu  mt3">
+                  <h4 className="mv0">{event.name}</h4>
+                  <span className="normal f7 gray">Event</span>
+                </div>
+                <div className="ttu mt2">
+                  <h4 className="mv0">{`${order.firstName} ${order.lastName}`}</h4>
+                  <span className="normal f7 gray">Name</span>
+                </div>
+                <div className="ttu fl mt2">
+                  <h4 className="mv0">{tix.ticketName}</h4>
+                  <span className="normal f7 gray">Ticket</span>
+                </div>
+                <div className="ttu fl mt2 ml2">
+                  <h4 className="mv0">{`${formatEventTime(
+                  new Date(event.startDate),
+                  new Date(event.endDate),
+                  event.location.timeZoneId,
+                )}`}</h4>
+                  <span className="normal f7 gray">Time</span>
+                </div>
+              </div>
+              <div className="v-mid fr">
+                <div className="">
+                  <QRCode
+                    value={`${tix.barCode}`}
+                    renderAs="svg"
+                    size={80}
+                  />
+                </div>
+                <div>{tix.barCode}</div>
+              </div>
+            </div>
+        </div>
+        <div className="dtc-l v-mid tr-l tc f2-l f3 fw6 mt3">
+          <span
+            onClick={() => setTixMode(false)}
+            className="no-underline white  noselect br-100 tc pa2 mr2 mt2-l ph4 mt2  bg-green"
+          >
+            Scan Another Tix
+          </span>
+        </div>
+      </main>
+      </div>}
     </div>
   );
 };
