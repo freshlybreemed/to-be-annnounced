38b1bafaa46350f43fd182514bda0818fc3782b7
ebrima <ebrima.jobe92@gmail.com>
2021-08-16 23:40:13 -0700

    settings basic info update

--- src/components/Dashboard/Settings/BasicInfo.tsx
+++ src/components/Dashboard/Settings/BasicInfo.tsx
@@ -1,15 +1,19 @@
 import axios from 'axios';
 import classNames from 'classnames';
-import * as React from 'react';
-import { useState } from 'react';
+import Cleave from 'cleave.js/react';
+import React, {useEffect, useState} from 'react';
 import {  UploadImage } from '..';
 import { UserProps } from '../../../@types/types';
+import 'cleave.js/dist/addons/cleave-phone.us';
+import { useRouter } from 'next/router'
 
 interface SettingsProps {
   userProp: UserProps;
 }
 export const BasicInfo: React.FunctionComponent<SettingsProps> = ({userProp}) => {
     const { settings } = userProp;
+    const router = useRouter()
+
     const [firstName, setFirstName] = useState<string>(settings.firstName || "");
     const [lastName, setLastName] = useState<string>(settings.lastName || "");
     const [phoneNumber, setPhoneNumber] = useState<string>(settings.phoneNumber || "");
@@ -33,6 +37,57 @@ export const BasicInfo: React.FunctionComponent<SettingsProps> = ({userProp}) =>
         return true;
     }
 
+    const socialsConfig = [
+        {
+            fields: [
+                {
+                    label: 'Website',
+                    name: 'website',
+                    value: website,
+                    set: setWebsite,
+                    required: false,
+                    error: false,
+                    errorText: 'Please enter in a valid website'
+                },
+                {
+                    label: 'Facebook',
+                    name: 'facebook',
+                    value: facebook,
+                    set: setFacebook,
+                    required: false,
+                    placeholder: 'facebook.com/',
+                    error: false,
+                    errorText: 'Please enter in a valid website'
+                }
+            ]
+
+        },
+        {
+            fields: [
+                {
+                    label: 'IG Handle',
+                    name: 'instagram',
+                    value: instagram,
+                    set: setInstagram,
+                    required: false,
+                    placeholder: '@',
+                    error: false,
+                    errorText: 'Please enter in a valid IG handle'
+                },
+                {
+                    label: 'Twitter Handle',
+                    name: 'twitter',
+                    value: twitter,
+                    set: setTwitter,
+                    required: false,
+                    error: false,
+                    placeholder: '@',
+                    errorText: 'Please enter in a valid Twitter handle'
+                }
+            ]
+
+        }
+    ]
     const inputConfig = [
         {
             fields: [
@@ -149,56 +204,10 @@ export const BasicInfo: React.FunctionComponent<SettingsProps> = ({userProp}) =>
             ]
 
         },
-        {
-            fields: [
-                {
-                    label: 'Website',
-                    name: 'website',
-                    value: website,
-                    set: setWebsite,
-                    required: false,
-                    error: false,
-                    errorText: 'Please enter in a valid website'
-                },
-                {
-                    label: 'Facebook',
-                    name: 'facebook',
-                    value: facebook,
-                    set: setFacebook,
-                    required: false,
-                    error: false,
-                    errorText: 'Please enter in a valid website'
-                }
-            ]
-
-        },
-        {
-            fields: [
-                {
-                    label: 'Instagram',
-                    name: 'instagram',
-                    value: instagram,
-                    set: setInstagram,
-                    required: false,
-                    error: false,
-                    errorText: 'Please enter in a valid IG handle'
-                },
-                {
-                    label: 'Twitter',
-                    name: 'twitter',
-                    value: twitter,
-                    set: setTwitter,
-                    required: false,
-                    error: false,
-                    errorText: 'Please enter in a valid Twitter handle'
-                }
-            ]
-
-        }
     ]
 
-    const handleSubmit = () =>{
-        setLoading(true)
+    const handleSubmit = async () =>{
+        setLoading(true);
         const user: UserProps = {
             ...userProp,
             settings:{
@@ -222,39 +231,66 @@ export const BasicInfo: React.FunctionComponent<SettingsProps> = ({userProp}) =>
                 }
             }
         }
-        axios
+        await axios
         .post('/api/user', {
             data: user
+        }).then(()=>{
+            router.reload();
         })
-        setLoading(false)
 
     };
 
     return (
-
         <div className={'w-100 ph4 pt4  bg-white black'}>
-            <div role="alert">
-                <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
-                    Danger
-                </div>
-                <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
-                    <p>Something not ideal might be happening.</p>
-                </div>
-            </div>
             <div className="w-70-ns w-100 center">
-                <div>
-                    <h1 className="tl fw7 mb0 pb3">Basic Info</h1>
+                <h2 className="tl fw7 mb0 pb3">Profile Photo</h2>
+                    <div className="mv4 pv2">
+                        <div className="mb5">
+                            {image && <img src={image} className="db w-100" />}
+                            <UploadImage setImage={setImage} />
+                                {/* <div className="py-20 h-screen bg-white px-2"> */}
+                                    {/* <div className="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl">
+                                        <div className="md:flex">
+                                            <div className="w-full p-3">
+                                                <div className="relative border-dotted h-48 rounded-lg border-dashed border-2 border-blue-700 bg-gray-100 flex justify-center items-center">
+                                                    <div className="absolute">
+                                                        <div className="flex flex-col items-center"> <i className="fa fa-folder-open fa-4x text-blue-700"></i> <span className="block text-gray-400 font-normal">Add a Profile Image</span> </div>
+                                                    </div> <input type="file" className="h-full w-full opacity-0" name=""/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div> */}
+                
+                        </div>
+                        
+                        <hr className="o-20 " />
+                    </div>
+                <div className="mb4 pv2">
+                    <h2 className="tl fw7 mb0 pb3">Personal Information</h2>
                     {inputConfig.map(fieldGroup=>{
                         const fieldOne = fieldGroup.fields[0]
                         if(fieldGroup.fields.length === 2) {
                             const fieldTwo = fieldGroup.fields[1]
                             return (
-                            <div className="md:my-3 w-100">
+                            <div key={fieldOne.name} className="md:my-3 w-100">
                                 <div className="dib md:w-2/4 w-full md:mb-0 mb-2 md:pr-2">
                                     <div className="tl ba-hover overflow-visible">
                                     <small className="db pl2 pt2 pb1 mid-gray ">
                                     {fieldOne.label} {fieldOne.required && <span className="red">*</span>}
                                     </small>
+                                    {fieldOne.name==="phoneNumber"?
+                                    
+                                    <Cleave
+                                        options={{ phone: true, phoneRegionCode: 'US' }}
+
+                                        value={fieldOne.value}
+                                        onChange={(event) => {
+                                        fieldOne.set(event.currentTarget.value);
+                                        checkForErrors({name:event.currentTarget.value})
+                                        }}
+                                        className="pl2 pb2  input-reset  bn w-90"
+                                    />:
                                     <input
                                         value={fieldOne.value}
                                         onChange={(event) => {
@@ -262,7 +298,7 @@ export const BasicInfo: React.FunctionComponent<SettingsProps> = ({userProp}) =>
                                         checkForErrors({name:event.currentTarget.value})
                                         }}
                                         className="pl2 pb2  input-reset  bn w-90"
-                                    />
+                                    />}
                                     </div>
                                 </div>
                                 <div className="dib  md:w-2/4 w-full md:mb-0 mb-2 ">
@@ -286,6 +322,7 @@ export const BasicInfo: React.FunctionComponent<SettingsProps> = ({userProp}) =>
                             return (
                                 <>
                                     <div
+                                    key={fieldOne.name} 
                                     className={`mb-2 tl ${classNames({
                                     'ba-hover': !settingsErrors.name,
                                     'ba-hover-error': settingsErrors.name,
@@ -312,31 +349,86 @@ export const BasicInfo: React.FunctionComponent<SettingsProps> = ({userProp}) =>
                     })}
                     <hr className="o-20 " />
                 </div>
-                <div className="mv4 pv2">
-                    <div className="mb5">
-                        {image && <img src={image} className="db w-100" />}
-                        <UploadImage setImage={setImage} />
-                    </div>
-                    <hr className="o-20 " />
-                </div>
-                <div className="py-20 h-screen bg-white px-2">
-                    <div className="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl">
-                        <div className="md:flex">
-                            <div className="w-full p-3">
-                                <div className="relative border-dotted h-48 rounded-lg border-dashed border-2 border-blue-700 bg-gray-100 flex justify-center items-center">
-                                    <div className="absolute">
-                                        <div className="flex flex-col items-center"> <i className="fa fa-folder-open fa-4x text-blue-700"></i> <span className="block text-gray-400 font-normal">Add a Profile Image</span> </div>
-                                    </div> <input type="file" className="h-full w-full opacity-0" name=""/>
+                <div className="mb4 pv2">
+                    <h2 className="tl fw7 mb0 pb3">Socials</h2>
+                    {socialsConfig.map(fieldGroup=>{
+                        const { placeholder,name,set,value,label, required, errorText } = fieldGroup.fields[0]
+                        if(fieldGroup.fields.length === 2) {
+                            const fieldTwo = fieldGroup.fields[1]
+                            return (
+                            <div key={name}  className="md:my-3 w-100">
+                                <div className="dib md:w-2/4 w-full md:mb-0 mb-2 md:pr-2">
+                                    <div className="tl ba-hover overflow-visible">
+                                    <small className="db pl2 pt2 pb1 mid-gray ">
+                                    {label} {required && <span className="red">*</span>}
+                                    </small>
+                                    <div className="pl2 pb2  input-reset  bn w-90">
+                                        {placeholder && <span>{placeholder}</span>}
+                                        <input
+                                            value={value}
+                                            onChange={(event) => {
+                                            set(event.currentTarget.value);
+                                            checkForErrors({name:event.currentTarget.value})
+                                            }}
+                                        />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="dib  md:w-2/4 w-full md:mb-0 mb-2 ">
+                                    <div className="tl ba-hover overflow-visible">
+                                        <small className="db pl2 pt2 pb1 mid-gray ">
+                                        {fieldTwo.label} {fieldTwo.required && <span className="red">*</span>}
+                                        </small>
+                                        <div className="pl2 pb2  input-reset  bn w-90">
+                                                                                                                {fieldTwo.placeholder && <span>{fieldTwo.placeholder}</span>}
+                                            <input
+                                                value={fieldTwo.value}
+                                                onChange={(event) => {
+                                                    fieldTwo.set(event.currentTarget.value);
+                                                    checkForErrors({name:event.currentTarget.value})
+                                                }}
+                                            />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <hr className="o-20" />
+                            
+                        )} else {
+                            return (
+                                <>
+                                    <div
+                                    key={name} 
+                                    className={`mb-2 tl ${classNames({
+                                    'ba-hover': !settingsErrors.name,
+                                    'ba-hover-error': settingsErrors.name,
+                                    })}`}
+                                    >
+                                        <small className="db pl2 pt2 pb1 mid-gray ">
+                                            {label} {required && <span className="red">*</span>}
+                                        </small>
+                                        <input
+                                            value={value}
+                                            onChange={(event) => {
+                                            set(event.currentTarget.value);
+                                            checkForErrors({name:event.currentTarget.value})
+                                            }}
+                                            className="pl2 pb2  input-reset  bn w-90"
+                                        />
+                                    </div>
+                                    {settingsErrors[name] && (
+                                    <small className="db tl red">{errorText}</small>
+                                    )}
+                                </>
+                            )
+                        }   
+                    })}
+                    <hr className="o-20 " />
+                </div>               
+               <hr className="o-20" />
             </div>
             <div
-            className="mv4 b--black ba bw1 dib noselect br-100 b--solid pa2 ph4 f3 fw5"
-            onClick={() => handleSubmit()}
+                className="mv4 b--black ba bw1 dib noselect br-100 b--solid pa2 ph4 f3 fw5"
+                onClick={() => handleSubmit()}
             >
             {loading && <i className="fa fa-spinner fa-spin mr2" />}
             {loading ? 'Saving...'
